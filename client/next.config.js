2c0e2ccbd59c8922f3920693ce9392b0996432fe
Sa4dUs <dmmarcelo27@gmail.com>
2022-06-13 00:46:35 +0200

    fix: :bug: build destination

--- client/next.config.js
+++ client/next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
-      },
-    ]
-  },
-}
+  reactStrictMode: true
+  }
 
 module.exports = nextConfig
